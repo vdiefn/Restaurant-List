bceb8997e8ff84f884d5c1b8f0838118fb3bdc49
vdiefn <ddhkxxhxx@hotmail.com>
2022-12-14 23:31:01 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -3,6 +3,23 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
+const mongoose = require('mongoose')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 
