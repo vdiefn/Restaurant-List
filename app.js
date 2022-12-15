217dc6d2d30750115d7c22f777c1e17d5321dafc
vdiefn <ddhkxxhxx@hotmail.com>
2022-12-15 21:32:06 +0800

    feat: mofidy index page & add create function

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const restaurantList = require('./restaurant.json')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const Restaurant = require('./models/restaurant')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -26,17 +28,52 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended:true}))
 
-
-app.get('/', (req, res) => {
-  res.render('index', {restaurants: restaurantList.results})
+//瀏覽所有餐廳
+app.get('/', (req,res) => {
+  Restaurant.find()
+  .lean()
+  .then((restaurants) => {
+    return res.render('index', {restaurants})
+  })
+  .catch(error => {
+    console.log(error)
+  })
 })
 
+
+//新增餐廳
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req,res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return Restaurant.create({name, name_en, category, image, location, phone, google_map, rating, description})
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(error => {
+      console.log(error)
+    })
+})
+
+//看特定餐廳的detail
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find(item => item.id.toString() === req.params.restaurant_id)
   res.render('show', {restaurant: restaurant})
 })
 
+//搜尋餐廳
 app.get('/search/', (req,res) =>{
   const keyword = req.query.keyword.trim().toLowerCase()
   const searchResult = restaurantList.results.filter( item => {
@@ -44,6 +81,10 @@ app.get('/search/', (req,res) =>{
   })
   res.render('index', { restaurants: searchResult })
 })
+
+
+
+
 
 app.listen( port, () => {
   console.log(`Express is running on http://localhost:${port}`)
