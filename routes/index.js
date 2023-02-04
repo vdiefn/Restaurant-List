296a82b7f26aa3c2a8467890a0ea7f21e7846709
vdiefn <ddhkxxhxx@hotmail.com>
2023-02-04 13:44:49 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const users = require('./modules/users')
 const router = express.Router()
 
 //引入 home 模組程式碼
@@ -7,10 +8,12 @@ const home = require('./modules/home')
 //引入 restaurants 模組程式碼
 const restaurants = require('./modules/restaurants')
 
+router.use('/users', users)
+//將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
+router.use('/restaurants', restaurants)
+
 //將網址結構符合 / 字串的 request 導向 home 模組 
 router.use('/', home)
 
-//將網址結構符合 /restaurants 字串開頭的 request 導向 restaurants 模組 
-router.use('/restaurants', restaurants)
 
 module.exports = router
