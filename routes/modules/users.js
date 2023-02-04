500d441321c5107596d56fdad77d7ed03b1394f1
vdiefn <ddhkxxhxx@hotmail.com>
2023-02-04 17:01:02 +0800

    feat: add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -16,9 +16,25 @@ router.post('/login', passport.authenticate('local', {
 //註冊
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if(!name || !email || !password || !confirmPassword){
+    errors.push({ message: '所有欄位都是必填的！'})
+  }
+  if(password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不相符！'})
+  }
+  if(errors.length){
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User already exists!')
+      errors.push({ message: '這個Email已經註冊過了！'})
       res.render('register', { 
         name, 
         email, 
@@ -44,6 +60,7 @@ router.get('/register', (req, res) => {
 //登出
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '你已經成功登出！')
   res.redirect('/users/login')
 })
 
